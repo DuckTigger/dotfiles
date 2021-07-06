8d8054cd095c6362b86d01256661bb9b088512e8
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2021-07-06 15:49:30 +0100

    add bat

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,9 @@
 alias ssh-myriad='TERM=xterm-256color ssh myriad'
 alias vim=nvim
 alias ls='lsd --group-dirs=first'
+alias ytop='ytop -p'
+alias btop=bpytop
+alias fzf="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+alias grep=batgrep
+alias less=bat
+alias cat=bat
