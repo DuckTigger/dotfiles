e36141daf7edd8dbc2756e92c53c6654a0579aef
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2021-10-20 13:23:50 +0100

    Arch wal update works

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,5 @@ alias btop=bpytop
 alias fzf="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 alias grep=batgrep
 alias less=bat
-alias cat=bat
 alias ipdf='mupdf -I -r 216'
 alias jupyterPort='nohup ssh -N -f -L localhost:9999:localhost:9999 Arch-PC'
