37a2b7c709027db0cd506cda14aaee7519b07009
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2021-11-18 14:29:28 +0000

    change alias to home PC

--- aliases.zsh
+++ aliases.zsh
@@ -7,4 +7,5 @@ alias fzf="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {
 alias grep=batgrep
 alias less=bat
 alias ipdf='mupdf -I -r 216'
-alias jupyterPort='nohup ssh -N -f -L localhost:9999:localhost:9999 Arch-PC'
+alias jupyterPortIn='nohup ssh -N -f -L localhost:9999:localhost:9999 Home-PC'
+alias jupyterPortExt='nohup ssh -N -f -L localhost:9999:localhost:9999 External-Home-PC'
